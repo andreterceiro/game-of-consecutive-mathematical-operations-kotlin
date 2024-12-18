033d49a3f63a77207e8927695dd2e3b7ea002990
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-18 10:23:56 -0300

    feat: manual tests

---
+++ tests/manuals/get-external-value1.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String>){
+    var first: Int
+    var second: Int
+    println("Enter the first number")
+    first = readLine()!!.toInt()
+    println("Enter the second number")
+    second= readLine()!!.toInt()
+    println("The sum is ${first + second}")
+}
---
+++ tests/manuals/get-external-value2.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    print("\nPlease insert 'p' for paper, 'r' for 'rock or 's' for 'scissors': ")
+
+    var userChoice = scan.nextLine()
+}
